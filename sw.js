a49668f2a16cf7d9b7da474e70f743b7028ca524
catejas <ca.tejasdesai@gmail.com>
2026-08-15 14:55:11 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* IPO Analyst service worker — offline shell */
-var CACHE = 'ipo-analyst-v1';
+var CACHE = 'ipo-analyst-v4';
 var ASSETS = [
   './', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png',
